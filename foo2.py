f7608a8aba9626981c2b98f9874f1daa64a96dcd
S2411678 <s2411678@bi.no>
2024-09-07 19:37:02 +0700

    modify foo2.py

--- foo2.py
+++ foo2.py
@@ -1,10 +1,5 @@
-#Excercise 2.10
-import sys
-cost = float(sys.argv[1])
-usage = float(sys.argv[2])
-price = float(sys.argv[3])
-power = float(sys.argv[4])
-year = float(sys.argv[5])
-resale = float(sys.argv[6])
-
-print(f"Total cost of owning a car for {year} years is {cost + (usage/power)*price*year - resale}")
+#Excercise 2.3
+x=float(input("x="))
+print("square of x is ",x*x)
+print("cube of x is ",x*x*x)
+print("fourth power of x is ",x**4)
